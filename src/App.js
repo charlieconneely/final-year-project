d66c1245a76ec4fc58dfcd1126df1e5b25d0b4f4
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-11-17 01:35:05 +0000

    canvas component created and displaying

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Canvas from './Canvas/canvas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Testing...</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas />
     </div>
   );
 }
---
+++ src/Canvas/canvas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Canvas = () => {
+  return (
+    <div>
+      <p>Canvas here...</p>
+    </div>
+  );
+}
+
+export default Canvas;
