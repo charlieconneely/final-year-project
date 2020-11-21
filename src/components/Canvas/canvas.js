a08c60b7ca726e0f6ab9bd6d6ed915e5d7865d7e
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-11-21 19:21:45 +0000

    basic canvas created, filled + stroked

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const Canvas = () => {
-  return (
-    <div>
-      <p>Canvas here...</p>
-    </div>
-  );
+const Canvas = (props) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.75;
+    canvas.height = window.innerHeight * 0.75;
+
+    const context = canvas.getContext('2d');
+
+    context.fillStyle = '#ff7e30';
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+    /* Outline canvas border */
+    context.strokeStyle = "#000000";
+    context.lineWidth = 5;
+    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+  }, []);
+
+  return <canvas  ref={canvasRef} {...props}/>
 }
 
 export default Canvas;
