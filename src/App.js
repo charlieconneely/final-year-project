a167192b7273831b095c38527b872d1d3559f213
unknown <g00361129@gmit.ie>
2020-11-24 02:16:00 +0000

    Added unique id generation to the server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Canvas from './components/Canvas/canvas';
+import Home from './components/Home/home';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/canvas" component={Canvas} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Videochat/videochat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoChat = () => {
+  return (
+    <div>
+      <h2>Video chat component, we made it boys :D</h2>
+    </div>
+  );
+}
+
+export default VideoChat;
