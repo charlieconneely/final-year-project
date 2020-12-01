8ce208e49d01cc89ac86a61fe6381527c548aaaf
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-12-01 16:20:48 +0000

    canvas.js converted to class with constructor + state

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React from 'react';
 import rough from 'roughjs/bundled/rough.esm';
 
 const generator = rough.generator();
@@ -8,78 +8,96 @@ function createElement(x1, y1, x2, y2) {
   return {x1, y1, x2, y2, roughElement};
 }
 
-const Canvas = () => {
+class Canvas extends React.Component {
 
-  const [elements, setElements] = useState([]);
-  const [isDrawing, setIsDrawing] = useState(false);
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.state,
+      isDrawing: null,
+      elements: []
+    }
+    this.drawOnCanvas = this.drawOnCanvas.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
 
-  useLayoutEffect(() => {
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext('2d');
+  componentDidMount () {
+    this.drawOnCanvas();
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.elements !== this.state.elements) {
+      this.drawOnCanvas();
+    }
+  }
+
+  drawOnCanvas() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+
     context.clearRect(0, 0, canvas.width, canvas.height);
-    const rc = rough.canvas(canvas);
+    var rc = rough.canvas(canvas);
 
-    const r1 = generator.rectangle(400, 200, 200, 250, {
-        fill: 'blue',
-        stroke: 'black',
-        hachureAngle: 60,
-        hachureGap: 10,
-        fillWeight: 5,
-        strokeWidth: 5
+    var r1 = generator.rectangle(400, 150, 100, 150, {
+      fill: 'blue',
+      hachureAngle: 60,
+      hachureGap: 10,
+      fillWeight: 5
     });
 
-    const r2 = generator.rectangle(600, 200, 200, 250, {
-        fill: 'white',
-        stroke: 'black',
-        hachureAngle: 60,
-        hachureGap: 10,
-        fillWeight: 5,
-        strokeWidth: 5
+    var r2 = generator.rectangle(500, 150, 100, 150, {
+      fill: 'white',
+      hachureAngle: 60,
+      hachureGap: 10,
+      fillWeight: 5
     });
 
     rc.draw(r1);
     rc.draw(r2);
 
-    elements.forEach(({roughElement}) => rc.draw(roughElement));
-  }, [elements]);
+    this.state.elements.forEach(({roughElement}) => rc.draw(roughElement));
+  }
 
-  const handleMouseDown = (event) => {
-    setIsDrawing(true);
-    const {clientX, clientY} = event;
-    const element = createElement(clientX, clientY, clientX, clientY);
-    setElements(prevState => [...prevState, element]);
-  };
+  handleMouseDown (event) {
+    var {clientX, clientY} = event;
+    var element = createElement(clientX, clientY, clientX, clientY);
+    this.setState(prevState => ({
+      elements: [...(prevState.elements || []), element]
+    }));
+    console.log(clientX, clientY);
+  }
 
-  const handleMouseMove = (event) => {
-    /* If drawing - track x and y coords */
-    if (!isDrawing) return;
+  /* 
+  to be used to actively draw the line before onMouseUp
+  issue - prevState is not itterable
+  handleMouseMove (event)  
+  */
 
-    const {clientX, clientY} = event;
+  handleMouseUp (event){
+    var {clientX, clientY} = event;
 
-    /* Retrieve last item on elements array */
-    const index = elements.length - 1;
-    const {x1, y1} = elements[index];
-    const currentElement = createElement(x1, y1, clientX, clientY);
+    /* Retrieve x,y coords of last item on elements array */
+    var index = this.state.elements.length - 1;
+    var {x1, y1} = this.state.elements[index];
+    var currentElement = createElement(x1, y1, clientX, clientY);
 
-    const elementsCopy = [...elements];
-    elementsCopy[index] = currentElement;
-    setElements(elementsCopy);
-  };
+    this.setState(prevState => ({
+      elements: [...(prevState.elements || []), currentElement]
+    }));
+  } 
 
-  const handleMouseUp = () => {
-    setIsDrawing(false);
-  };
-
-  return (
-    <canvas id="canvas"
-            style={{backgroundColor:'grey'}}
-            width={window.innerWidth}
-            height={window.innerHeight}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}>
-    </canvas>
-  );
+  render () {
+    return (
+      <canvas id="canvas"
+        style={{backgroundColor:'grey'}}
+        width={window.innerWidth}
+        height={window.innerHeight * 0.75}
+        onMouseDown={e => this.handleMouseDown(e)}
+        onMouseUp={e => this.handleMouseUp(e)}>
+      </canvas>
+    );
+  }
 }
 
 export default Canvas;
