8015753714d96549a0ac5063300ed588317d4103
unknown <g00361129@gmit.ie>
2020-12-16 12:21:21 +0000

    Added server.js, script.js and room.ejs

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const { v4: uuidV4 } = require("uuid")
 
-app.set('view engine', 'ejs')
+// Template created by the server creates
+// (might change from ejs to JSX/react? or send data to a certain page)
+app.set("view engine", "ejs")
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
-    res.render('room', {
+app.get("/:room", (req, res) => {
+    res.render("room", {
         roomId: req.params.room
+    })
+})
+
+io.on("connection", socket => {
+    //Pass the roomId and userId when a user joins a room
+    socket.on("join-room", (roomId, userId) => {
+        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit("user-connected", userId)
+        
+        socket.on("disconnect", () =>{
+            socket.to(roomId).broadcast.emit("user-disconnected", userId)
+        })
     })
 })
 
