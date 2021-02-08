bfbba5794bc8422ac49e42d14aeb3cec05d0a591
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-02-08 19:50:40 +0000

    Square drawing option working

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -4,8 +4,15 @@ import './canvas.css'
 
 const generator = rough.generator();
 
-function createElement(x1, y1, x2, y2) {
-  const roughElement = generator.line(x1, y1, x2, y2);
+function createElement(shape, x1, y1, x2, y2) {
+  var roughElement;
+  
+  if (shape === "Square") {
+    roughElement = generator.rectangle(x1, y1, x2 - x1, y2 - y1)
+  } else {
+    roughElement = generator.line(x1, y1, x2, y2)
+  }
+
   return {x1, y1, x2, y2, roughElement};
 }
 
@@ -17,6 +24,7 @@ class Canvas extends React.Component {
     super(props);
     this.state = {
       ...this.state,
+      shape: "Line",
       elements: []
     }
     this.drawOnCanvas = this.drawOnCanvas.bind(this);
@@ -52,7 +60,7 @@ class Canvas extends React.Component {
     this.isDrawing = true;
     var {clientX, clientY} = event;
     console.log(clientX + " " + clientY);
-    var element = createElement(clientX-50, clientY, clientX-50, clientY);
+    var element = createElement(this.state.shape, clientX-50, clientY, clientX-50, clientY);
     this.defineLineEdges(element);
   }
 
@@ -63,11 +71,11 @@ class Canvas extends React.Component {
     /* Retrieve x,y coords of last item on elements array */
     var index = this.state.elements.length - 1;
     var {x1, y1} = this.state.elements[index];
-    var currentElement = createElement(x1, y1, clientX-50, clientY);
+    var currentElement = createElement(this.state.shape, x1, y1, clientX-50, clientY);
     const elementsCopy = [...this.state.elements];
     elementsCopy[index] = currentElement;
 
-    this.setState({elements: elementsCopy});
+    this.setState({...this.state, elements: elementsCopy});
   }
 
   handleMouseUp (event){
@@ -77,13 +85,14 @@ class Canvas extends React.Component {
     /* Retrieve x,y coords of last item on elements array */
     var index = this.state.elements.length - 1;
     var {x1, y1} = this.state.elements[index];
-    var currentElement = createElement(x1, y1, clientX-50, clientY);
+    var currentElement = createElement(this.state.shape, x1, y1, clientX-50, clientY);
 
     this.defineLineEdges(currentElement);
   }
 
   defineLineEdges(element) {
     this.setState(prevState => ({
+      ...prevState.shape,
       elements: [...(prevState.elements || []), element]
     }));
   }
@@ -95,14 +104,14 @@ class Canvas extends React.Component {
 
   loadNewCanvas(event) {
     event.preventDefault();
-    var e1 = createElement(365, 166, 695, 66);
-    var e2 = createElement(375, 216, 719, 106);
+    var e1 = createElement("Line", 365, 166, 695, 66);
+    var e2 = createElement("Line", 375, 216, 719, 106);
 
-    this.setState({elements: [...this.state.elements, e1, e2]})
+    this.setState({...this.state, elements: [...this.state.elements, e1, e2]})
   }
 
-  changeShape(event) {
-    console.log(event.target.value);
+  changeShape = (event) => {
+    this.setState({...this.state, shape: event.target.value});
   }
 
   render () {
@@ -117,7 +126,7 @@ class Canvas extends React.Component {
             onMouseUp={e => this.handleMouseUp(e)}>
           </canvas>
         </div>
-        <div onChange={this.changeShape}>
+        <div onChange={e => this.changeShape(e)}>
           <input type="radio" value="Line" name="Choice" defaultChecked/> Line
           <input type="radio" value="Square" name="Choice"/> Square
         </div>
