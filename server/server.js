359750870f6ed434460501b856c2d17da8b0c6e7
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-02-20 16:23:53 +0000

    installed socketio-client dependency

--- server/server.js
+++ server/server.js
@@ -7,22 +7,22 @@ const { v4: uuidV4 } = require("uuid")
 // Page created by the server (in the '/views' folder)
 // (might change from ejs to JSX/react? or send data to a certain page)
 // https://github.com/reactjs/express-react-views
-app.set("view engine", "ejs")
+//app.set("view engine", "ejs")
 
 // Javascript + css folder
-app.use(express.static("public"))
+/*  app.use(express.static("public"))  */
 
 //Redirects the user to a dynamically created room (uuid url)
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.redirect(`/${uuidV4()}`)
-})
+})*/
 
 //Renders "room.ejs", redirects the user to it, passing through the "uuid" roomID.
-app.get("/:room", (req, res) => {
+/*app.get("/:room", (req, res) => {
     res.render("room", {
         roomId: req.params.room
     })
-})
+})*/
 
 //Event listener for when user connects to localhost:3000
 io.on("connection", socket => {
@@ -38,7 +38,13 @@ io.on("connection", socket => {
             socket.to(roomId).broadcast.emit("user-disconnected", userId)
         })
     })
+
+    socket.on("canvas-change", ({name, canvas}) => {
+        io.emit(canvas, {name, canvas})
+    })
 })
 
 //Application hosted on 'localhost:3000'
-server.listen(3000)
+server.listen(3000, function() {
+    console.log("Listening on port 3000")
+})
