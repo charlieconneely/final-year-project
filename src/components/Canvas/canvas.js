291b74eea47844efcf808c1b89055356108eca1d
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-02-22 17:16:14 +0000

    Canvas abstracted to child class drawingBoard.js

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -1,21 +1,18 @@
-import React, {useState,useLayoutEffect, useEffect, useRef} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import uuid from 'react-uuid'
-import rough from 'roughjs/bundled/rough.esm'
 import io from 'socket.io-client'
 import useLocalStorage from '../../hooks/useLocalStorage'
-import shapeGenerator from './shapeGenerator'
 import './canvas.css'
+import DrawingBoard from './drawingBoard'
 
 function Canvas(props) {
 
   const [isDrawing, setIsDrawing] = useState(false)
   const [shape, setShape] = useState("Line")
-  const [elements, setElements] = useLocalStorage("elements", [])
-  const [yourID, setYourID] = useLocalStorage('userID', '')
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
   const [inControl, setControl] = useState(false)
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [elements, setElements] = useLocalStorage("elements", [])
+  const [yourID, setYourID] = useLocalStorage('userID', '')
 
   const socketRef = useRef()
 
@@ -29,10 +26,6 @@ function Canvas(props) {
 
     socketRef.current.on("your id", id => {
       console.log("Socket ID: " + id)
-    })
-
-    socketRef.current.on("message", message => {
-      setMessages(oldies => [...oldies, message])
     })
 
     socketRef.current.on("control switch", id => {
@@ -62,16 +55,6 @@ function Canvas(props) {
     setWindowWidth(window.innerWidth)
   }
 
-  function sendMessage(e) {
-    e.preventDefault();
-    const messageObject = {
-      body: message,
-      id: yourID
-    }
-    setMessage('')
-    socketRef.current.emit("send message", messageObject)
-  }
-
   function switchControl(e) {
     e.preventDefault();
     if (inControl) {
@@ -79,80 +62,6 @@ function Canvas(props) {
       return
     }
     socketRef.current.emit("take control", yourID)
-  }
-
-  useLayoutEffect(() => {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.lineWidth = 5;
-    context.strokeStyle="black";
-    context.strokeRect(0, 0, canvas.width, canvas.height);
-    context.font = '28px serif';
-    
-    var rc = rough.canvas(canvas)
-
-    elements.forEach(e => {
-      /* check if item is a text element */
-      if (e.hasOwnProperty('type')) {
-        context.fillText(e.val, e.xco, e.yco);
-        return;
-      }
-      rc.draw(e.roughElement)
-    });
-    
-  }, [elements, windowWidth])
-
-  const handleMouseDown = (event) => {
-    var {pageX, pageY} = event;
-    var xPos = pageX - document.getElementById('canvas').offsetLeft;
-    var yPos = pageY - document.getElementById('canvas').offsetTop;
-
-    /* if text is selected - add textElement obj to state with value from input field */
-    if (shape === "Text") {
-      var textInputElement = {
-        type:"Text",
-        val:document.getElementById('inputText').value,
-        xco:xPos,
-        yco:yPos
-      }
-      setElements(prevState => [...prevState, textInputElement])
-      return;
-    }
-
-    setIsDrawing(true)
-    const element = shapeGenerator(shape, xPos, pageY, xPos, yPos);
-    setElements(prevState => [...prevState, element])
-  }
-
-  const handleMouseMove =(event) => {
-    if (!isDrawing) return;
-
-    var {pageX, pageY} = event;
-    // Account for offset
-    var xPos = pageX - document.getElementById('canvas').offsetLeft;
-    var yPos = pageY - document.getElementById('canvas').offsetTop;
-    // Retrieve x,y coords of last item on elements array
-    var index = elements.length - 1;
-    var {x1, y1} = elements[index];
-
-    var currentElement = shapeGenerator(shape, x1, y1, xPos, yPos);
-    const elementsCopy = [...elements];
-    elementsCopy[index] = currentElement;
-
-    setElements(elementsCopy)
-  }
-
-  const handleMouseUp = () => {
-    setIsDrawing(false)
-    if (!inControl) return;
-    // if in control - send updated elements array across socket
-    const canvasObject = {
-      body: elements,
-      id: yourID
-    }
-    socketRef.current.emit("send canvas state", canvasObject)
   }
 
   const undo = (event) => {
@@ -174,8 +83,8 @@ function Canvas(props) {
     setShape(event.target.value)
   }
 
-  const onMessageChange = (event) => {
-    setMessage(event.target.value)
+  const sendCanvas = (canvasObject) => {
+    socketRef.current.emit("send canvas state", canvasObject)
   }
 
   const controlButtonMessage = inControl ? 'Stop Controlling' : 'Take Control'
@@ -189,13 +98,12 @@ function Canvas(props) {
   return (
     <div>
       <div>
-        <canvas id="canvas"
-          width={window.innerWidth - 100}
-          height={window.innerHeight - 200}
-          onMouseDown={e => handleMouseDown(e)}
-          onMouseMove={e => handleMouseMove(e)}
-          onMouseUp={handleMouseUp}>
-        </canvas>
+        <DrawingBoard sendCanvasState={sendCanvas}
+            propsInControl={inControl}
+            isPropsDrawing={isDrawing} setIsPropsDrawing={setIsDrawing}
+            id={yourID} 
+            propsElements={elements} setPropsElements={setElements}
+            propsShape={shape} winWidth={windowWidth}/>
       </div>
       <div>
         <button onClick={e => switchControl(e)}>{controlButtonMessage}</button>
@@ -207,10 +115,6 @@ function Canvas(props) {
       </div>
       <div>
         {canvasTextInput}
-      </div>
-      <div>
-        <input name="message" onChange={e=>onMessageChange(e)} value={message} placeholder="Type something..."/>
-        <button onClick={e => sendMessage(e)}>Send</button>
       </div>
       <div>
         <button onClick={e => undo(e)}>Undo</button>
---
+++ src/components/Canvas/drawingBoard.js
@@ -0,0 +1,94 @@
+import React, {useLayoutEffect} from 'react'
+import shapeGenerator from './shapeGenerator'
+import rough from 'roughjs/bundled/rough.esm'
+
+function DrawingBoard (props) {
+
+    useLayoutEffect(() => {
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext('2d');
+    
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.lineWidth = 5;
+        context.strokeStyle="black";
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+        context.font = '28px serif';
+        
+        var rc = rough.canvas(canvas)
+    
+        props.propsElements.forEach(e => {
+          /* check if item is a text element */
+          if (e.hasOwnProperty('type')) {
+            context.fillText(e.val, e.xco, e.yco);
+            return;
+          }
+          rc.draw(e.roughElement)
+        });
+        
+    }, [props.propsElements, props.winWidth])
+
+    const handleMouseDown = (event) => {
+        var {pageX, pageY} = event;
+        var xPos = pageX - document.getElementById('canvas').offsetLeft;
+        var yPos = pageY - document.getElementById('canvas').offsetTop;
+    
+        /* if text is selected - add textElement obj to state with value from input field */
+        if (props.propsShape === "Text") {
+          var textInputElement = {
+            type:"Text",
+            val:document.getElementById('inputText').value,
+            xco:xPos,
+            yco:yPos
+          }
+          props.setPropsElements(prevState => [...prevState, textInputElement])
+          return;
+        }
+    
+        props.setIsPropsDrawing(true)
+        const element = shapeGenerator(props.propsShape, xPos, pageY, xPos, yPos);
+        props.setPropsElements(prevState => [...prevState, element])
+    }
+
+    const handleMouseMove = (event) => {
+        if (!props.isPropsDrawing) return;
+    
+        var {pageX, pageY} = event;
+        // Account for offset
+        var xPos = pageX - document.getElementById('canvas').offsetLeft;
+        var yPos = pageY - document.getElementById('canvas').offsetTop;
+        // Retrieve x,y coords of last item on elements array
+        var index = props.propsElements.length - 1;
+        var {x1, y1} = props.propsElements[index];
+    
+        var currentElement = shapeGenerator(props.propsShape, x1, y1, xPos, yPos);
+        const elementsCopy = [...props.propsElements];
+        elementsCopy[index] = currentElement;
+    
+        props.setPropsElements(elementsCopy)
+      }
+
+    const handleMouseUp = () => {
+        props.setIsPropsDrawing(false)
+        if (!props.propsInControl) return;
+        // if in control - send updated elements array across socket
+        const canvasObject = {
+            body: props.propsElements,
+            id: props.id
+        }
+        props.sendCanvasState(canvasObject)
+    }
+
+    return (
+        <div>
+            <canvas id="canvas"
+                width={window.innerWidth - 100}
+                height={window.innerHeight - 200}
+                onMouseDown={e => handleMouseDown(e)}
+                onMouseMove={e => handleMouseMove(e)}
+                onMouseUp={handleMouseUp}>
+            </canvas>
+        </div>
+    )
+}
+
+export default DrawingBoard
