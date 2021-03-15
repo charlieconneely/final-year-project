3d83b5215e6216c5771f5e14d1426e92d2491178
unknown <g00361129@gmit.ie>
2021-03-15 20:17:03 +0000

    Cleaned up/Removed redundant code/comments, cleaned up server code

--- server.js
+++ server.js
@@ -16,36 +16,6 @@ app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/build/index.html')
 })
 
-// *** CANVAS SOCKETS NEEDS TO BE MODIFIED ***
-//Event listener for when user connects to localhost:3000
-io.on("connection", socket => {
-    //Pass the roomId and userId when a user joins a room
-    socket.on("join-room", (roomId, userId) => {
-        console.log(roomId, userId)
-        //join the room with the 
-        socket.join(roomId)
-        //Broadcast the "user-connected" event, to trigger a trigger in "script.js" 
-        socket.to(roomId).broadcast.emit("user-connected", userId)
-        //When the user disconnects, broadcast the "user-disconnected" event, to trigger a trigger in "script.js" 
-        socket.on("disconnect", () =>{
-            socket.to(roomId).broadcast.emit("user-disconnected", userId)
-        })
-    })
-    
-    socket.emit("your id", socket.id)
-
-    socket.on("send message", body => {
-        io.emit('message', body)
-    })
-
-    socket.on("send canvas state", body => {
-        io.emit('canvasState', body)
-    })
-
-    socket.on("take control", userID => {
-        io.emit('control switch', userID)
-    })
-})
 // Application hosted on 'localhost:{port}'
 const server = app.listen(port, () => console.log("Listening on port: ", port))
 
@@ -96,6 +66,21 @@ peers.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('disconnected')
         peerConnections.delete(socket.id)
+    })
+
+    // Canvas section
+    socket.emit("your id", socket.id)
+
+    socket.on("send message", body => {
+        io.emit('message', body)
+    })
+
+    socket.on("send canvas state", body => {
+        io.emit('canvasState', body)
+    })
+
+    socket.on("take control", userID => {
+        io.emit('control switch', userID)
     })
 
 })
