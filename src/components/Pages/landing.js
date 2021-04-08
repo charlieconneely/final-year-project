c331f95a21bc09171a591b0057dceb42a1c313ee
Charlie Conneely <charlieconneely.cc@gmail.com>
2021-04-08 23:04:37 +0100

    Got landing page UI looking better

--- src/components/Pages/landing.js
+++ src/components/Pages/landing.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { Button } from '@material-ui/core'
+import React from 'react'
+import { Button, TextField } from '@material-ui/core'
+import './landing.css'
 
 function LandingPage() {
 
     return (
-        <div>
-            <Button color="secondary">Host Room</Button>
-            <Button color="secondary">Join Room</Button>
+        <div class='container'>
+            <div class='option'>
+                <h2>Welcome to Videsign</h2>
+            </div>
+            <div class='option'>
+                <Button variant='outlined' size='large'>Host Room</Button>
+            </div>
+            <div class='option'>
+                <TextField autoComplete="off"
+                    label="Enter room ID here" /> 
+                <Button variant='outlined' size='large'>Join Room</Button>
+            </div>
         </div>
     )
 }
